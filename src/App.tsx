59cad71e9c48328f30222c57867c7d84e3e3fcf7
maritocuate <mario300@gmail.com>
2024-04-27 16:48:19 -0300

    ✨ home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import '@fontsource/bebas-neue'
 import './App.css'
 import Gradient from './components/Gradient'
+import Home from './components/Home'
 
 function App() {
   return (
     <>
       <Gradient />
-      <div className="section">
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            textAlign: 'right',
-          }}
-        >
-          <h1 className="header">
-            Mario
-            <br />
-            Quiroga
-          </h1>
-          <p style={{ marginTop: '-4.5rem', fontSize: '0.8rem' }}>
-            software developer
-          </p>
-        </div>
-      </div>
+      <Home />
     </>
   )
 }
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,16 @@
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className="section">
+      <section className="title-section">
+        <h1 className="header">
+          Mario
+          <br />
+          Quiroga
+        </h1>
+        <p className="subtitle">software developer</p>
+      </section>
+    </div>
+  )
+}
