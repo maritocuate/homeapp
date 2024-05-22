5817d967dc002f1f87c44ebf184e291fbfb9edaf
maritocuate <mario300@gmail.com>
2024-05-22 18:10:53 -0300

    ✨ pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'en',
+        name: 'Mario Quiroga | Software Developer',
+        short_name: 'Mario Quiroga',
+        description: "Mario Quiroga's web portfolio",
+        theme_color: '#98509f',
+        background_color: '#98509f',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
