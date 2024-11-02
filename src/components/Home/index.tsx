8a59f00d3a5808b93a4de15e3384bf0409639ba0
maritocuate <mario300@gmail.com>
2024-11-02 14:58:20 -0300

    ✨ 3d header effect

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from 'react'
 import './Home.css'
 
 export default function Home() {
+  const headerRef = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    const text = headerRef.current
+    if (text) {
+      let shadow = ''
+      for (let i = 1; i < 17; i++) {
+        shadow += `${shadow ? ',' : ''} ${-i + 1}px ${i + 1}px 0 #d9d9d9`
+      }
+      text.style.textShadow = shadow
+    }
+  }, [])
   return (
     <div className="section">
-      <section className="title-section">
-        <h1 className="header">
+      <section className="title-section dimension">
+        <h1 ref={headerRef} className="header">
           Mario
           <br />
           Quiroga
