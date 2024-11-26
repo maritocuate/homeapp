f44dea5867223c930240d57799fef6298ce9b556
maritocuate <mario300@gmail.com>
2024-11-26 19:48:28 -0300

    ✨ skills icons update

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import * as Tone from 'tone'
 import './Skills.css'
 
 const socialMedia = [
@@ -13,14 +14,27 @@ const socialMedia = [
   { type: 'postgresql' },
   { type: 'swift' },
 ]
+const odeToJoy = ['E4', 'E4', 'F4', 'G4', 'G4', 'F4', 'E4', 'D4']
 export default function Skills() {
   const [pressedIndex, setPressedIndex] = useState<null | number>(null)
+  const [indexTheme, setIndexTheme] = useState(0)
+  const synth = new Tone.Synth().toDestination()
+  const now = Tone.now()
 
   const handlePressStart = (index: number) => {
     setPressedIndex(index)
+    setIndexTheme(indexTheme + 1)
+    if (indexTheme === odeToJoy.length - 1) setIndexTheme(0)
+    playNote()
   }
   const handlePressEnd = () => {
     setPressedIndex(null)
+  }
+
+  const playNote = () => {
+    console.log(indexTheme)
+    synth.triggerAttack(odeToJoy[indexTheme], now)
+    synth.triggerRelease(now + 0.5)
   }
 
   return (
