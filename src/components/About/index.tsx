f933a56bd1f8c4e6b15a2f06bfffc95f816c07e5
maritocuate <mario300@gmail.com>
2024-11-29 20:07:31 -0300

    chore(about): copy update

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,12 +6,12 @@ export default function About() {
       <section className="about-section">
         <h1 className="header">About me</h1>
         <p className="about-description">
-          Hi! My name is Mario and I live in Argentina. Ive been programming for
-          about 10 years (when Flash sites were very popular). Over the past
-          three yeard, I've been extensively working with React and NextJs to
-          keep the same language in the backend. In addition to my frontend
-          expertise, I have hands-on experience with both relational and
-          non-relational databases. In my life I have worked in product and
+          Hi! My name is Mario and I live in Buenos Aires, Argentina. Ive been
+          programming for about 10 years (when Flash sites were very popular).
+          Over the past three yeard, I've been extensively working with React
+          and NextJs to keep the same language in the backend. In addition to my
+          frontend expertise, I have hands-on experience with both relational
+          and non-relational databases. In my life I have worked in product and
           service companies in teams with great professionals and diverse web
           and mobile challenges. Always trying to use the latest libraries and
           technologies. I really love to develop software and keep myself
