d33e074e5c5addab99c4ea2bc678de1ebea2bfd8
maritocuate <mario300@gmail.com>
2024-12-10 12:16:19 -0300

    chore(public): public directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  publicDir: 'public',
   plugins: [
     react(),
     VitePWA({
